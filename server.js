7f3b69f58a4a21ee0d210486369bbd663ee8cf4e
AdeSantosSierra <a.desantos.sierra@gmail.com>
2025-04-11 00:33:55 +0200

    Incorporo los cambios con NoticiasProcesadas

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
+// Para ejecutar -> nodemon server.js
+// Para subir a render -> Commit, push y esperar a que haga deploy
+// Si has cambiado .env -> Hay que actualizar "Environment" en render
+
+
 // Cargar las variables de entorno
 require('dotenv').config();
+console.log("URI:", process.env.MONGO_URI);
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("✅ Conectado a MongoDB");
+})
+.catch(err => {
+  console.error("❌ Error al conectar a MongoDB:", err.message);
+});
+
+
 
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
-const Noticia = require('./models/Articles'); 
-const Noticias = require('./models/Noticia'); 
+console.log("Ejecutando server hola.js");
+// const mongoose = require('mongoose');
+// const Noticia = require('./models/Articles'); 
+
+// const Noticias = require('./models/Noticia'); 
+const NoticiasProcesadas = require('./models/NoticiaProcesada'); 
 const app = express();
 const port = process.env.PORT || 3000;
+
+console.log("Ejecutando server.js");
 
 // Conexión a MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI, {
@@ -83,11 +108,13 @@ app.get('/url', async (req, res) => {
 
     console.log('por aquí pasa')
     console.log(query)
-    console.log(query)
+
 
     // Obtener los artículos de la base de datos
-    const articles = await Noticias.findOne(query);
+    
+    const articles = await NoticiasProcesadas.findOne(query);
 
+    console.log('por aquí pasa 0')
     console.log(articles)
 
     console.log('por aquí pasa 1')
@@ -99,41 +126,7 @@ app.get('/url', async (req, res) => {
   }
 });
 
-// Endpoint GET para obtener artículos con filtros opcionales
-app.get('/ident', async (req, res) => {
-  const identificador = req.query.identificador
 
-  console.log('req.query')
-  console.log(req.query)
-  console.log(req.query.identificador)
-  console.log(identificador)
-
-
-
-  try {
-    // Construir los criterios de búsqueda
-    let query = {};
-
-    // Filtrar por título usando regex (sin importar mayúsculas/minúsculas)
-    query = {identificador}
-
-    console.log('por aquí pasa')
-    console.log(query)
-    console.log(query)
-
-    // Obtener los artículos de la base de datos
-    const articles = await Noticias.findOne(query);
-
-    console.log(articles)
-
-    console.log('por aquí pasa 1')
-
-    // Enviar los artículos como respuesta
-    res.status(200).json(articles);
-  } catch (err) {
-    res.status(500).json({ message: 'Error obteniendo los artículos', error: err});
-  }
-});
 
 // Iniciar el servidor en el puerto especificado
 app.listen(port, () => {
