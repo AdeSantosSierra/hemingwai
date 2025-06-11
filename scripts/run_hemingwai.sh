e96602f6a1c07835b63593185d9311a24a98de25
Alberto <alberto.desantos@ufv.es>
2025-06-11 19:44:46 +0000

    Add run_hemingwai.sh script

---
+++ scripts/run_hemingwai.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# (1) Activar el entorno virtual
+source /home/alberto/envs/hemingwai/bin/activate
+
+# (2) Ir al directorio del proyecto
+cd /home/alberto/projects/hemingwai/src
+
+# (3) Ejecutar el script Python
+python Periodico.py 
