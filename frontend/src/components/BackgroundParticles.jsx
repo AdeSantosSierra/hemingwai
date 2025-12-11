c915994b4bb993b76f1cba40c1d05521ccd1ecdd
Rober <161973696+RAvila-bioeng@users.noreply.github.com>
2025-12-11 11:24:56 +0100

    cambio estilo visual de las partículas de la web"

--- frontend/src/components/BackgroundParticles.jsx
+++ frontend/src/components/BackgroundParticles.jsx
@@ -13,10 +13,10 @@ const BackgroundParticles = () => {
     
     // Configuración
     const GRID_SIZE = 60; // Debe coincidir con el background-size del CSS
-    const PARTICLE_COUNT = 5; // Cantidad de partículas
+    const PARTICLE_COUNT = 4; // Cantidad de partículas
     const PARTICLE_COLOR = '#d2d209'; // Lima
     const SPEED = 0.5; // Velocidad de movimiento
-    const TRAIL_LENGTH = 100; // Longitud de la estela
+    const TRAIL_LENGTH = 300; // Longitud de la estela
 
     // Ajustar tamaño del canvas
     const resizeCanvas = () => {
